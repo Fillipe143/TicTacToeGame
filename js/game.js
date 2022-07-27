548fe229f79ca7539b086d0015b2efc5d900bf2a
Fillipe143 <fillipedev.contato@gmail.com>
2022-07-27 13:51:04 -0300

    added game class

---
+++ js/game.js
@@ -0,0 +1,39 @@
+export class Game {
+    #players = ['X', 'Ｏ']
+    #gameOver = false
+
+    constructor() {
+        this.#gameOver = false
+        this.board = new Array(9)
+        this.round = 0
+    }
+
+    play(position) {
+        if (this.#gameOver) return
+        if (this.board[position] != undefined) return
+
+        this.board[position] = this.#players[this.round]
+        this.round = this.round === 1 ? 0 : 1
+    }
+
+    getWinningPositions() {
+        let winningPositions = []
+
+        for (let i = 0; i < 9; i += 3)
+            checkIfEquals(0 + i, 1 + i, 2 + i)
+
+        for (let i = 0; i < 3; i++)
+            checkIfEquals(0 + i, 3 + i, 6 + i)
+
+        for (let i = 0; i < 4; i += 2)
+            checkIfEquals(4, 0 + i, 8 - i)
+
+
+        function checkIfEquals(p1, p2, p3) {
+            if (this.board[p1] != undefined && this.board[p1] == this.board[p2] && this.board[p2] == this.board[p3])
+                winningPositions.push(p1, p2, p3)
+        }
+
+        return winningPositions
+    }
+}
