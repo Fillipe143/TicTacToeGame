e1eedb172144d910f218b52fb4956ae39721e802
Fillipe143 <fillipedev.contato@gmail.com>
2022-07-27 14:16:59 -0300

    bugs solved

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@ export class Game {
     #players = ['X', 'Ｏ']
 
     constructor() {
-        this.#gameOver = false
+        this.winningPositions = []
         this.board = new Array(9)
         this.round = 0
     }
@@ -12,26 +12,25 @@ export class Game {
 
         this.board[position] = this.#players[this.round]
         this.round = this.round === 1 ? 0 : 1
+
+        this.#updateWinningPositions()
     }
 
-    getWinningPositions() {
-        let winningPositions = []
+    #updateWinningPositions() {
+        this.winningPositions = []
 
         for (let i = 0; i < 9; i += 3)
-            checkIfEquals(0 + i, 1 + i, 2 + i)
+            this.#checkIfEquals(0 + i, 1 + i, 2 + i)
 
         for (let i = 0; i < 3; i++)
-            checkIfEquals(0 + i, 3 + i, 6 + i)
+            this.#checkIfEquals(0 + i, 3 + i, 6 + i)
 
         for (let i = 0; i < 4; i += 2)
-            checkIfEquals(4, 0 + i, 8 - i)
+            this.#checkIfEquals(4, 0 + i, 8 - i)
+    }
 
-
-        function checkIfEquals(p1, p2, p3) {
-            if (this.board[p1] != undefined && this.board[p1] == this.board[p2] && this.board[p2] == this.board[p3])
-                winningPositions.push(p1, p2, p3)
-        }
-
-        return winningPositions
+    #checkIfEquals(p1, p2, p3) {
+        if (this.board[p1] != undefined && this.board[p1] == this.board[p2] && this.board[p2] == this.board[p3])
+            this.winningPositions.push(p1, p2, p3)
     }
 }
